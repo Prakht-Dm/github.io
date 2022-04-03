76d9c3123e5557bed78fa695f0bff3379620d3bf
DMITRY PRAKHT <98157604+Prakht-Dm@users.noreply.github.com>
2022-04-03 13:45:30 +0700

    Make SNAKE

--- main.js
+++ main.js
@@ -1,31 +1,119 @@
-async function whatGender(event){
-    event.preventDefault();
-    let firstName = this.firstElementChild.value;
-    let serverUrl = 'https://api.genderize.io';
-    let url = `${serverUrl}?name=${firstName}`;
-    let response = await fetch(url);
-if (response.ok) { 
-  let json = await response.json();
-  let outGender = document.createElement('p');
-        outGender.className='listString'; 
-        outGender.innerHTML=`${firstName} is a ${json['gender']} name`;
-    if(json['gender']){
-        this.after(outGender);
-    }else {
-        outGender.innerHTML=`${firstName} is not found`;  
-        this.after(outGender);
-}
-} else {
-  alert("ERROR: Name not found");
-}
-this.firstElementChild.value="";
+const DEAFAUL_SNAKE_POSITION = [41,21,1];
+let snake = DEAFAUL_SNAKE_POSITION.slice(0);
+let fruitPosition;
+let currentScore = 0;
+const SNAKE_DIRECTIONS = {
+    NONE: 'NONE',
+    TOP: 'TOP',
+    BOTTOM: 'BOTTOM',
+    RIGHT: 'RIGHT',
+    LEFT: 'LEFT',
+};
+let currentSnakeDirection = SNAKE_DIRECTIONS.BOTTOM; 
+let pressedDirection = SNAKE_DIRECTIONS.BOTTOM;
+
+
+createFruit();
+createField(400);
+showScore(currentScore);
+
+let runGame = setInterval(changeSnakePosition, 100);
+
+document.addEventListener('keydown', changeSnakeDirection);
+
+
+function changeSnakeDirection(pressedButton) {
+    if (pressedButton.code === "KeyS" && currentSnakeDirection !== SNAKE_DIRECTIONS.TOP) pressedDirection = SNAKE_DIRECTIONS.BOTTOM; 
+    if (pressedButton.code === "KeyD" && currentSnakeDirection !== SNAKE_DIRECTIONS.LEFT) pressedDirection = SNAKE_DIRECTIONS.RIGHT; 
+    if (pressedButton.code === "KeyW" && currentSnakeDirection !== SNAKE_DIRECTIONS.BOTTOM) pressedDirection = SNAKE_DIRECTIONS.TOP; 
+    if (pressedButton.code === "KeyA" && currentSnakeDirection !== SNAKE_DIRECTIONS.RIGHT) pressedDirection = SNAKE_DIRECTIONS.LEFT; 
+    }
+
+function createField(counter){
+        if (counter === 0) return
+        let square = document.createElement("div");
+        (snake.includes(counter)) ? square.className = "snake_body" : square.className= "field_Square";
+        if (counter === snake[0]) square.innerHTML="^_^";
+        if (counter === fruitPosition) square.innerHTML = "&#127822;";
+        mainFrame.prepend(square);
+        createField(counter-1);
+        currentSnakeDirection=pressedDirection;
+    }
+
+function changeSnakePosition(){
+    mainFrame.innerHTML = "";
+    let snakeTail = snake.slice(0);
+
+if (currentSnakeDirection === SNAKE_DIRECTIONS.TOP) {
+    snake=snake.map((item, index)=>{
+        if (index === 0 ) {
+            if (item-20 < 0) {return item+380
+        } else {
+            return item-20 }}
+        return snakeTail[index-1];       
+    }) 
 }
 
-function addNewEvent(classN, events, func){
-    let crossCheck=document.querySelectorAll(classN);
-    for (let k of crossCheck){
-    k.addEventListener(events,  func);
-    } 
+if (currentSnakeDirection === SNAKE_DIRECTIONS.BOTTOM) {
+    snake=snake.map((item, index)=>{
+        if (index === 0 ) {
+            if (item+20 > 400) {return item-380
+        } else {
+            return item+20 }}
+        return snakeTail[index-1];       
+    }) 
 }
-addNewEvent('form','submit', whatGender);
 
+if (currentSnakeDirection === SNAKE_DIRECTIONS.RIGHT) {
+    snake=snake.map((item, index)=>{
+        if (index === 0) {
+            if (item%20 === 0) {return item-19
+        } else {
+            return item+1 }}
+        return snakeTail[index-1];       
+    }) 
+}
+
+if (currentSnakeDirection === SNAKE_DIRECTIONS.LEFT) {
+    snake=snake.map((item, index)=>{
+        if (index === 0) {
+            if ((item+19)%20 === 0) {return item+19
+        } else {
+            return item-1 }}
+        return snakeTail[index-1];       
+    }) 
+}
+
+if (snake[0] === fruitPosition) makeSnakeLonger(snakeTail);
+if (snake.slice(1).includes(snake[0])) gameOver();
+
+createField(400);
+}
+
+function makeSnakeLonger(snakeTail){
+    snake.push(snakeTail[snakeTail.length])
+    createFruit();
+    showScore(++currentScore);
+}
+
+function createFruit() {
+    fruitPosition= Math.floor(Math.random() * 399)+1;
+    if (snake.includes(fruitPosition)) return createFruit()
+  }
+
+function showScore(score){
+    scoreBar.innerHTML="";
+    let scoreOut = document.createElement("p");
+    scoreOut.innerHTML=`Score: ${score}`;
+    scoreBar.append(scoreOut);
+}
+
+function gameOver(){
+    currentSnakeDirection = SNAKE_DIRECTIONS.NONE;  
+    clearInterval(runGame);
+   alert(`GAME OVER!!\nYou ate ${currentScore} fruits\nPress Ok to play again`);
+   showScore(currentScore=0);
+   snake = DEAFAUL_SNAKE_POSITION.slice(0);
+   currentSnakeDirection = SNAKE_DIRECTIONS.BOTTOM; 
+   runGame = setInterval(changeSnakePosition, 100);
+}
